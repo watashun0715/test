07159ce6ad6936caeffc1755d52fb7de1cdb345d
watashun0715 <93954771+watashun0715@users.noreply.github.com>
2021-11-10 08:59:42 +0900

    修正11/10 map, filterの処理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import "./styles.css";
+// const array = [1, 2, 3];
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // const [num1, ...num2] = array;
+
+// const func1 = (num1 = 0) => console.log(num1);
+
+// func1();
+
+// const name = ["田中", "山田", "鈴木"];
+
+// const newName = name.map ((name, index) => {
+//   if (name.indexOf("田") === -1) {
+//     console.log(`${name}です。出席番号は、${index + 1}です。`)
+//   } else {
+//     console.log(`私は${name}です。出席番号は${index + 1}です。名前に"田"があります。`);
+//   }
+// })
+
+// console.log(newName);
+
+// const num = [1, 2, 3, 4, 5];
+
+// const numFunc = num.filter((num1) => {
+//   return num1 % 2 === 1;
+// })
+
+// console.log(numFunc);
