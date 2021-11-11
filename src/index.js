10c3e17cee01f6a23cc614196835a3c8fcadcd14
watashun0715 <93954771+watashun0715@users.noreply.github.com>
2021-11-12 08:31:27 +0900

    削除機能完了、完了機能途中

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("textAdd").value;
-  alert(inputText);
+  document.getElementById("textAdd").value = "";
+
+  // li作成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list_row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  // 完了ボタン生成
+  const comp = document.createElement("button");
+  comp.innerText = "完了";
+  comp.addEventListener("click", () => {
+    // TODO項目を未完了リストから削除
+    deleteFromIncompleteList(comp.closest("li"));
+    // 完了ボタンの親要素(div)を取得
+    const addTarget = comp.parentNode;
+    // TODO項目のテキスト内容を取得
+    const text = addTarget.firstElementChild.innerText;
+  });
+
+  // 削除ボタン生成
+  const del = document.createElement("button");
+  del.innerText = "削除";
+  del.addEventListener("click", () => {
+    // 押下された削除ボタンの親要素(div)を未完了リストから削除する。
+    deleteFromIncompleteList(del.closest("li"));
+  });
+
+  div.appendChild(p);
+  div.appendChild(comp);
+  div.appendChild(del);
+
+  li.appendChild(div);
+
+  document.getElementById("incomplete_list").appendChild(li);
+};
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete_list").removeChild(target);
 };
 
 document
