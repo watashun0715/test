2ac3d9b064d30239bdf06778700f618e44071895
watashun0715 <93954771+watashun0715@users.noreply.github.com>
2021-11-11 23:36:57 +0900

    moromoro

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
-// const array = [1, 2, 3];
+const onClickAdd = () => {
+  const inputText = document.getElementById("textAdd").value;
+  alert(inputText);
+};
 
-// // const [num1, ...num2] = array;
-
-// const func1 = (num1 = 0) => console.log(num1);
-
-// func1();
-
-// const name = ["田中", "山田", "鈴木"];
-
-// const newName = name.map ((name, index) => {
-//   if (name.indexOf("田") === -1) {
-//     console.log(`${name}です。出席番号は、${index + 1}です。`)
-//   } else {
-//     console.log(`私は${name}です。出席番号は${index + 1}です。名前に"田"があります。`);
-//   }
-// })
-
-// console.log(newName);
-
-// const num = [1, 2, 3, 4, 5];
-
-// const numFunc = num.filter((num1) => {
-//   return num1 % 2 === 1;
-// })
-
-// console.log(numFunc);
+document
+  .getElementById("buttonAdd")
+  .addEventListener("click", () => onClickAdd());
